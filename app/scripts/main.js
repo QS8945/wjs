a2a752caedc8c07eed74b944c6a435122db44692
QS8945 <1119225756@qq.com>
2017-07-31 05:14:00 +0800

    轮播图标签页卡券制作完成

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,19 @@
 'use strict';
-console.log('show');
+$(function(){
+	
+	//轮播图自适应
+	function resize(){
+		var windowWidth = $(window).width();
+		var isSmallScreen = windowWidth<768;
+		$('#carousel-example-generic > .carousel-inner >.item').each(function(index,val){
+			var $item = $(val);
+			$item.css('background-image','url("'+$item.data(isSmallScreen ? 'image-xs' : 'image-lg')+'")');
+			if(isSmallScreen){
+				$item.html('<img src="'+$item.data('image-xs')+'" alt="轮播图" />')
+			}else{
+				$item.empty();
+			}
+		});
+	}
+	$(window).on('resize',resize).trigger('resize');
+});
