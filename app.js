21b981da9d5c30059c258073bf89bf37ed191a64
QS8945 <1119225756@qq.com>
2017-08-01 21:14:10 +0800

    微金所发布

---
+++ app.js
@@ -0,0 +1,7 @@
+'use strict';
+
+// 加载CSS
+//require('bootstrap-loader');
+require('./../bower_components/bootstrap/dist/css/bootstrap.css');
+require('./../bower_components/jquery/dist/jquery.js');
+require('./../bower_components/bootstrap/dist/js/bootstrap.js');
